a2c5565231cf819ab3ce8a4c491e185dfc7e913e
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-08-26 14:53:51 +0600

    final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => (
       textAlign="center"
       pb="40px"
     >
-      Made with ❤️ by JavaScript Mastery
+      Made with ❤️ by Mahesh Biswas
     </Typography>
   </Box>
 );
